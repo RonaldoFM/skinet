91aab642b71b408044fe7c31d411e4580d872188
RonaldoFM <43863628+RonaldoFM@users.noreply.github.com>
2020-05-01 20:08:20 -0300

    End of Section 6

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -11,6 +11,7 @@ using API.Dtos;
 using AutoMapper;
 using API.Errors;
 using Microsoft.AspNetCore.Http;
+using API.Helpers;
 
 namespace API.Controllers
 {
@@ -20,7 +21,6 @@ namespace API.Controllers
         private readonly IGenericRepository<ProductType> _productType;
         private readonly IGenericRepository<ProductBrand> _productBrand;
         private readonly IMapper _mapper;
-
 
         public ProductsController(IGenericRepository<Product> productRepo,
                                   IGenericRepository<ProductBrand> productBrand,
@@ -35,12 +35,24 @@ namespace API.Controllers
         }
 
         [HttpGet]
-        public async Task<ActionResult<IReadOnlyList<ProductToReturnDto>>> GetProducts()
+        public async Task<ActionResult<Pagination<ProductToReturnDto>>> GetProducts(
+            [FromQuery]ProductSpecParams productParams)
         {
-            var spec = new ProductsWithTypesAndBrandsSpecification();
+            var spec = new ProductsWithTypesAndBrandsSpecification(productParams);
+
+            var countSpec = new ProductWithFiltersForCountSpecifications(productParams);
+
+            var totalItems = await _productRepo.CountAsync(countSpec);
+
             var products = await _productRepo.ListAsync(spec);
 
-            return Ok(_mapper.Map<IReadOnlyList<Product>, IReadOnlyList<ProductToReturnDto>>(products));
+            var data = _mapper.Map<IReadOnlyList<Product>, IReadOnlyList<ProductToReturnDto>>(products);
+
+            return Ok(new Pagination<ProductToReturnDto>(
+                            productParams.PageIndex,
+                            productParams.PageSize,
+                            totalItems,
+                            data));
         }
 
         [HttpGet("{id}")]
